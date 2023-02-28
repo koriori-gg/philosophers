989334e9fc9d8ca21731637f8de77d5a87606106
koriori <maaacha.kuri05@gmail.com>
2023-02-28 16:03:25 +0900

    clean: ファイル構造の変更

--- philo/main.c
+++ philo/main.c
@@ -1,6 +1,24 @@
 #include "philo.h"
 
+void *do_something(void *arg)
+{
+	printf("do something\n");
+	sleep(1);
+	return (arg);
+}
+
 int main(void)
 {
-	printf("start");
+   pthread_t thread;
+   void *retval;
+
+   // スレッドを作成
+   if (pthread_create(&thread, NULL, do_something, NULL) != 0)
+       // エラー処理
+       return (0);
+   // スレッドの終了方法
+   // 1. スレッドが終了するまで待つ
+   pthread_join(thread, &retval);
+   // 2. スレッドを待たない
+   pthread_detach(thread);
 }
