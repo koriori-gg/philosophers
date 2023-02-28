c442f14247a7ddfa579e481b4d838cedb960f5dc
koriori <maaacha.kuri05@gmail.com>
2023-02-28 17:07:01 +0900

    update: sleep eat think

--- philo/main.c
+++ philo/main.c
@@ -1,24 +1,56 @@
 #include "philo.h"
 
-void *do_something(void *arg)
+void *philo(void *arg)
 {
-	printf("do something\n");
+	printf("I am Philo\n");
+	printf("%s: %p\n", __func__, pthread_self());
 	sleep(1);
 	return (arg);
 }
 
-int main(void)
+void eating(int time)
 {
-   pthread_t thread;
-   void *retval;
+	usleep(time * 1000);
+}
+
+void sleeping(int time)
+{
+	usleep(time * 1000);
+}
+
+void thinking(int time)
+{
+	usleep(time * 1000);
+}
+
+//number_of_philosophers time_to_die time_to_eat time_to_sleep
+int main(int argc, char **argv)
+{
+	pthread_t	*thread;
+	int			count;
+	int			i;
+	void 		*retval;
 
    // スレッドを作成
-   if (pthread_create(&thread, NULL, do_something, NULL) != 0)
+   	count = atoi(argv[1]);
+	thread = (pthread_t)malloc(sizeof(pthread_t) * count);
+	i = 0;
+	while(i < count)
+	{
+		if (pthread_create(&thread[i], NULL, philo, NULL) != 0)
+			// エラー処理
+			return (0);
+		i++;
+	}
+
+	if (pthread_create(&thread, NULL, philo, NULL) != 0)
        // エラー処理
        return (0);
-   // スレッドの終了方法
-   // 1. スレッドが終了するまで待つ
-   pthread_join(thread, &retval);
-   // 2. スレッドを待たない
-   pthread_detach(thread);
+	// スレッドの終了方法
+	// 1. スレッドが終了するまで待つ
+	pthread_join(thread, &retval);
+	pthread_join(thread, &retval);
+	// 2. スレッドを待たない
+	pthread_detach(thread);
+	pthread_detach(thread);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,5 +6,6 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
+# include <sys/time.h>
 
 #endif
