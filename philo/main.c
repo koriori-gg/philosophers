ac1ccb81b6b3b721f8fc3424ff3a8ef57d3639de
koriori <maaacha.kuri05@gmail.com>
2023-03-02 09:44:50 +0900

    add: error_exit

--- philo/main.c
+++ philo/main.c
@@ -23,19 +23,37 @@ void thinking(int time)
 	usleep(time * 1000);
 }
 
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	error_exit(char *message, int status)
+{
+	write(1, message, ft_strlen(message));
+	exit(status);
+}
+
 //number_of_philosophers time_to_die time_to_eat time_to_sleep
 int main(int argc, char **argv)
 {
 	pthread_t	*thread;
 	int			count;
 	int			i;
+	pthread_mutex_t mutex;
 	void 		*retval;
 
-   // スレッドを作成
-   if (argc != 5)
-		printf("argument error");
-   	count = atoi(argv[1]);
+	// スレッドを作成
+	if (argc != 5)
+		error_exit("argument error\n", EXIT_FAILURE);
+	count = atoi(argv[1]);
 	thread = (pthread_t *)malloc(sizeof(pthread_t) * count);
+	pthread_mutex_init(&mutex, NULL);
 	i = 0;
 	while(i < count)
 	{
@@ -56,7 +74,7 @@ int main(int argc, char **argv)
 	i = 0;
 	while(i < count)
 	{
-			pthread_detach(thread[i]);
+		pthread_detach(thread[i]);
 		i++;
 	}
 }
