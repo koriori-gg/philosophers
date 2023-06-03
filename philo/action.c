8d3fbd18ef89f742c596b1058c9352a6014592d9
koriori <maaacha.kuri05@gmail.com>
2023-06-03 22:55:19 +0900

    add: 行動用の関数の作成

---
+++ philo/action.c
@@ -0,0 +1,19 @@
+#include "philo.h"
+
+void	philo_eat(t_philo *philo)
+{
+	(void)philo;
+	printf("is eating\n");
+}
+
+void	philo_sleep(t_philo *philo)
+{
+	(void)philo;
+	printf("is sleeping\n");
+}
+
+void	philo_think(t_philo *philo)
+{
+	(void)philo;
+	printf("is thinking\n");
+}
