32c49038661e1f6652454c30e9d10fa70517e71f
koriori <maaacha.kuri05@gmail.com>
2023-06-12 17:36:15 +0900

    refactor: time.cの不要なコメントの削除

--- philo/time.c
+++ philo/time.c
@@ -5,7 +5,6 @@ long	get_time(void)
 	struct timeval tv;
 
 	gettimeofday(&tv, NULL);
-	// printf("%ld\n", tv.tv_sec * 1000 +  tv.tv_usec / 1000);
 	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
