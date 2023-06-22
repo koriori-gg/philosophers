d1660bd1410f2bdc8ed908795ab6e34250182819
koriori <maaacha.kuri05@gmail.com>
2023-06-22 16:43:55 +0900

    add: pthread_createを追加

--- philo/ft_pthread.c
+++ philo/ft_pthread.c
@@ -12,14 +12,20 @@ void	ft_pthread_mutex_unlock(pthread_mutex_t *mtx)
 		exit(1);
 }
 
-void	ft_pthread_join(pthread_t thread)
-{
-	if (pthread_join(thread, NULL) != 0)
-		exit(1);
-}
-
 void	ft_pthread_mutex_destroy(pthread_mutex_t *mtx)
 {
 	if (pthread_mutex_destroy(mtx) != 0)
+		exit(1);
+}
+
+void	ft_pthread_create(pthread_t *thread, void *func, t_simulation *philo_i)
+{
+	if (pthread_create(thread, NULL, func, philo_i) != 0)
+		exit(1);
+}
+
+void	ft_pthread_join(pthread_t thread)
+{
+	if (pthread_join(thread, NULL) != 0)
 		exit(1);
 }
