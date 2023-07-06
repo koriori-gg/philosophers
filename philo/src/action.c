4772f7c85327aec5c009d26005b926e75fde603c
koriori <maaacha.kuri05@gmail.com>
2023-07-07 00:02:29 +0900

    update: think,sleepのmutexをなくす

--- philo/src/action.c
+++ philo/src/action.c
@@ -7,8 +7,8 @@ int	philo_eat(t_philo *philo)
 	if (philo_take_fork(philo) == -1)
 		return (-1);
 	now = get_time();
-	pthread_mutex_lock(&(philo->philo_mutex));
 	change_state(philo, EAT);
+	pthread_mutex_lock(&(philo->philo_mutex));
 	update_last_eat_time(philo, now);
 	print_message(philo, philo->id, now, "is eating");
 	add_eat_count(philo);
@@ -25,11 +25,9 @@ int	philo_sleep(t_philo *philo)
 
 	if (should_stop(philo))
 		return (-1);
-	pthread_mutex_lock(&(philo->philo_mutex));
 	change_state(philo, SLEEP);
 	now = get_time();
 	print_message(philo, philo->id, now, "is sleeping");
-	pthread_mutex_unlock(&(philo->philo_mutex));
 	wait_time(now, philo->simulation->time_to_sleep);
 	return (0);
 }
@@ -41,10 +39,8 @@ int	philo_think(t_philo *philo)
 	if (should_stop(philo))
 		return (-1);
 	now = get_time();
-	pthread_mutex_lock(&(philo->philo_mutex));
 	change_state(philo, THINK);
 	print_message(philo, philo->id, now, "is thinking");
-	pthread_mutex_unlock(&(philo->philo_mutex));
 	wait_time(now, philo->next_eat_time - now);
 	if (philo->id == 1 && philo->simulation->num_philo == 1 && philo->one_philo)
 		wait_time(now, philo->simulation->time_to_die);
