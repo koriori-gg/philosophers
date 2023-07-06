8c96967e091053784f81c531324436e7ba2d71f6
koriori <maaacha.kuri05@gmail.com>
2023-07-06 11:47:00 +0900

    refactor: 消し忘れのwait_timeの削除

--- philo/src/init.c
+++ philo/src/init.c
@@ -31,7 +31,6 @@ static void	init_philo(t_simulation *simulation)
 		simulation->philo[i].id = i + 1;
 		simulation->philo[i].eat_count = 0;
 		simulation->philo[i].last_eat_time = simulation->start;
-		// simulation->philo[i].wait_time = set_wait_time(simulation, i + 1);
 		simulation->philo[i].next_eat_time = simulation->start + set_wait_time(simulation, i + 1);
 		pthread_mutex_init(&(simulation->philo[i].l_fork), NULL);
 		if (i > 0)
