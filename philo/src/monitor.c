37bf23ac6efa2f64e2f97b1b2ff96079dd0cc62c
koriori <maaacha.kuri05@gmail.com>
2023-07-11 14:34:10 +0900

    update: monitorの判定に食事回数を追加

--- philo/src/monitor.c
+++ philo/src/monitor.c
@@ -6,36 +6,46 @@
 /*   By: ihashimo <ihashimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 11:33:07 by ihashimo          #+#    #+#             */
-/*   Updated: 2023/07/11 11:33:08 by ihashimo         ###   ########.fr       */
+/*   Updated: 2023/07/11 14:30:12 by ihashimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static bool	stop_philo(t_simulation *simulation, int i)
+{
+	pthread_mutex_lock(&(simulation->monitor->stop_mutex));
+	if (i >= 0)
+		print_dead(get_time() - simulation->start,
+			simulation->philo[i].id, "is dead");
+	simulation->monitor->stop = true;
+	pthread_mutex_unlock(&(simulation->monitor->stop_mutex));
+	return (false);
+}
 
 bool	check_philo(t_simulation *simulation)
 {
 	long	i;
 	bool	need;
 	long	now;
+	long	eat_count;
 
 	i = 0;
 	need = true;
+	eat_count = 0;
 	while (i < simulation->num_philo && need)
 	{
 		pthread_mutex_lock(&(simulation->philo[i].philo_mutex));
 		now = get_time();
 		if (now - simulation->philo[i].last_eat_time >= simulation->time_to_die)
-		{
-			pthread_mutex_lock(&(simulation->monitor->stop_mutex));
-			print_dead(get_time() - simulation->start,
-				simulation->philo[i].id, "is dead");
-			simulation->monitor->stop = true;
-			need = false;
-			pthread_mutex_unlock(&(simulation->monitor->stop_mutex));
-		}
+			need = stop_philo(simulation, i);
+		if (simulation->philo[i].eat_count >= simulation->must_eat)
+			eat_count += 1;
 		pthread_mutex_unlock(&(simulation->philo[i].philo_mutex));
 		i++;
 	}
+	if (simulation->must_eat != -1 && eat_count == simulation->num_philo)
+		need = 	stop_philo(simulation, -1);
 	return (need);
 }
 
